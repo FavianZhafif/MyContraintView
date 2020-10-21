94adb7fe1b2bcceb80bbc5268ceb833337df621b
FavianZhafif <zhafif.cerdas@gmail.com>
2020-10-21 14:41:23 +0700

    MyContraintApp

---
+++ app/src/main/java/com/dicoding/picodiploma/myconstraintview/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.dicoding.picodiploma.myconstraintview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_specification)
+        setContentView(R.layout.activity_main)
+
+    }
+}
